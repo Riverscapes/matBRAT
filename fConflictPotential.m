84d52f28329fd075456ba47430ffc47e86c541f8
Wally Macfarlane <wally.macfarlane@gmail.com>
2014-05-27 14:51:47 -0600

    Fixed minor typos in the comments while reviewing the code for potential errors.

--- fConflictPotential.m
+++ fConflictPotential.m
@@ -12,7 +12,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 
 %
-% This function outuputs a probablity of conflict potential
+% This function outputs a probablity of conflict potential
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [oPC_Prob] = fConflictPotential(iPC_UDotX,iPC_RoadX,iPCRoadAdj,iPC_RR, iPC_Canal)
@@ -43,62 +43,62 @@ AdjLow = 100;
 AdjHigh = 750;
 
 if iPC_UDotX >= 0 && iPC_UDotX <= CrossingLow
-    UDotX_Prob = 0.9; % Asign high probability when crossing in reach
+    UDotX_Prob = 0.9; % Assign high probability when crossing in reach
 elseif iPC_UDotX > CrossingLow && iPC_UDotX <= CrossingHigh
-    UDotX_Prob = -0.0011867*iPC_UDotX + 0.90118667; % Asign high probability when crossing in reach
+    UDotX_Prob = -0.0011867*iPC_UDotX + 0.90118667; % Assign high probability when crossing in reach
 elseif iPC_UDotX > CrossingHigh
-    UDotX_Prob = 0.01; % Asign high probability when crossing in reach
+    UDotX_Prob = 0.01; % Assign high probability when crossing in reach
 else
-    UDotX_Prob = 0.01; % Asign very low probability otherwise
+    UDotX_Prob = 0.01; % Assign very low probability otherwise
 end
 % Road Crossings
 if iPC_RoadX >= 0 && iPC_RoadX <= CrossingLow
-    RoadX_Prob = 0.9; % Asign high probability when crossing in reach
+    RoadX_Prob = 0.9; % Assign high probability when crossing in reach
 elseif iPC_RoadX > 100 && iPC_RoadX <= CrossingHigh
-    RoadX_Prob = -0.0011867*iPC_RoadX + 0.90118667; % Asign high probability when crossing in reach
+    RoadX_Prob = -0.0011867*iPC_RoadX + 0.90118667; % Assign high probability when crossing in reach
 elseif iPC_RoadX > CrossingHigh
-    RoadX_Prob = 0.01; % Asign high probability when crossing in reach
+    RoadX_Prob = 0.01; % Assign high probability when crossing in reach
 else
-    RoadX_Prob = 0.01; % Asign very low probability otherwise
+    RoadX_Prob = 0.01; % Assign very low probability otherwise
 end
 
 % Road Adjacent
 if iPCRoadAdj >= 0 && iPCRoadAdj <= AdjLow
-    RoadAdj_Prob = 0.9; % Asign high probability when crossing in reach
+    RoadAdj_Prob = 0.9; % Assign high probability when crossing in reach
 elseif iPCRoadAdj > AdjLow && iPCRoadAdj <= AdjHigh
-    RoadAdj_Prob = -0.00137*iPCRoadAdj + 1.0369; % Asign high probability when crossing in reach
+    RoadAdj_Prob = -0.00137*iPCRoadAdj + 1.0369; % Assign high probability when crossing in reach
 elseif iPCRoadAdj > AdjHigh
-    RoadAdj_Prob = 0.01; % Asign high probability when crossing in reach
+    RoadAdj_Prob = 0.01; % Assign high probability when crossing in reach
 else
-    RoadAdj_Prob = 0.01; % Asign very low probability otherwise
+    RoadAdj_Prob = 0.01; % Assign very low probability otherwise
 end
 
 % Canal
 if iPC_Canal >= 0 && iPC_Canal <= AdjLow
-    Canal_Prob = 0.9; % Asign high probability when crossing in reach
+    Canal_Prob = 0.9; % Assign high probability when crossing in reach
 elseif iPC_Canal > AdjLow && iPC_Canal <= AdjHigh
-    Canal_Prob = -0.00137*iPC_Canal + 1.0369; % Asign high probability when crossing in reach
+    Canal_Prob = -0.00137*iPC_Canal + 1.0369; % Assign high probability when crossing in reach
 elseif iPC_Canal > AdjHigh
-    Canal_Prob = 0.01; % Asign high probability when crossing in reach
+    Canal_Prob = 0.01; % Assign high probability when crossing in reach
 else
-    Canal_Prob = 0.01; % Asign very low probability otherwise
+    Canal_Prob = 0.01; % Assign very low probability otherwise
 end
 
 % Rail Road
 if iPC_RR >= 0 && iPC_RR <= AdjLow
-    RR_Prob = 0.9; % Asign high probability when crossing in reach
+    RR_Prob = 0.9; % Assign high probability when crossing in reach
 elseif iPC_RR > AdjLow && iPC_RR <= AdjHigh
-    RR_Prob = -0.00137*iPC_RR + 1.0369; % Asign high probability when crossing in reach
+    RR_Prob = -0.00137*iPC_RR + 1.0369; % Assign high probability when crossing in reach
 elseif iPC_RR > AdjHigh
-    RR_Prob = 0.01; % Asign high probability when crossing in reach
+    RR_Prob = 0.01; % Assign high probability when crossing in reach
 else
-    RR_Prob = 0.01; % Asign very low probability otherwise
+    RR_Prob = 0.01; % Assign very low probability otherwise
 end
 
 %% Combine probability
 ProbArray = [UDotX_Prob RoadX_Prob RoadAdj_Prob RR_Prob Canal_Prob];
 
-oPC_Prob = max(ProbArray); % Just let worse probabilty drive model
+oPC_Prob = max(ProbArray); % Just let worst probabilty drive model
     
 return
 
