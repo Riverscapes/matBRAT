9f1e1c19f2661f2550fe6bef3bfd04e20cc8c25b
Martha Jensen <martlajensen@gmail.com>
2014-06-12 16:56:26 -0600

    Conflict potential formulas were changed

--- fConflictPotential.m
+++ fConflictPotential.m
@@ -33,42 +33,48 @@ end
 if iPC_Canal < 0
     iPC_Canal = 10000;
 end
+if iPC_LU < 0
+    iPC_LU = 10000;
+end
+if iPC_Own < 0
+    iPC_Own = 10000;
+end
 
 
 %% Use Transform Functions to estimate probabilities
 % UDOT
 CrossingLow = 50;
 CrossingHigh = 1000;
-AdjLow = 100;
+AdjLow = 10;
 AdjHigh = 750;
 
 if iPC_UDotX >= 0 && iPC_UDotX <= CrossingLow
     UDotX_Prob = 0.9; % Assign high probability when crossing in reach
 elseif iPC_UDotX > CrossingLow && iPC_UDotX <= CrossingHigh
-    UDotX_Prob = -0.0011867*iPC_UDotX + 0.90118667; % Assign high probability when crossing in reach
+    UDotX_Prob = -0.0011867*iPC_UDotX + 0.90118667; % Use formula to calculate probability
 elseif iPC_UDotX > CrossingHigh
-    UDotX_Prob = 0.01; % Assign high probability when crossing in reach
+    UDotX_Prob = 0.01; % Assign low probability when UDOT culvert is far from reach
 else
     UDotX_Prob = 0.01; % Assign very low probability otherwise
 end
 % Road Crossings
 if iPC_RoadX >= 0 && iPC_RoadX <= CrossingLow
     RoadX_Prob = 0.9; % Assign high probability when crossing in reach
-elseif iPC_RoadX > 100 && iPC_RoadX <= CrossingHigh
-    RoadX_Prob = -0.0011867*iPC_RoadX + 0.90118667; % Assign high probability when crossing in reach
+elseif iPC_RoadX > CrossingLow && iPC_RoadX <= CrossingHigh
+    RoadX_Prob = -0.0011867*iPC_RoadX + 0.90118667; % Use formula to calculate probability
 elseif iPC_RoadX > CrossingHigh
-    RoadX_Prob = 0.01; % Assign high probability when crossing in reach
+    RoadX_Prob = 0.01; % Assign low probability when crossing is far from reach
 else
     RoadX_Prob = 0.01; % Assign very low probability otherwise
 end
 
 % Road Adjacent
 if iPCRoadAdj >= 0 && iPCRoadAdj <= AdjLow
-    RoadAdj_Prob = 0.9; % Assign high probability when crossing in reach
+    RoadAdj_Prob = 0.9; % Assign high probability when road is close to reach
 elseif iPCRoadAdj > AdjLow && iPCRoadAdj <= AdjHigh
-    RoadAdj_Prob = -0.0100933333333*iPCRoadAdj + 1.00933333333; % Assign high probability when crossing in reach
+    RoadAdj_Prob = -0.0100933333333*iPCRoadAdj + 1.00933333333; % Use formula to calculate probability
 elseif iPCRoadAdj > AdjHigh
-    RoadAdj_Prob = 0.01; % Assign high probability when crossing in reach
+    RoadAdj_Prob = 0.01; % Assign low probability when road is far from reach
 else
     RoadAdj_Prob = 0.01; % Assign very low probability otherwise
 end
